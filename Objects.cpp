8e48124de5beea9d13d79d319bf29f16d57632f7
MazeN Hassn <mazenby6@gmail.com>
2023-03-05 11:28:51 +0200

    Sprites added

--- Objects.cpp
+++ Objects.cpp
@@ -1,6 +1,6 @@
 #include "SFML/Graphics.hpp"
 #include "SFML/Audio.hpp"
-#include <iostream>
+#include<iostream>
 #define MENU_SIZE 3
 #define START_BUTTON 0
 #define OPTIONS_BUTTON 1
@@ -11,7 +11,9 @@ using namespace sf;
 
 int main()
 {
-	string options[MENU_SIZE] = {"START", "OPTIONS", "EXIT"};
+	string options[MENU_SIZE] = { "START", "OPTIONS", "EXIT" };
+
+
 
 	RenderWindow window(VideoMode(1920, 1080), "SFML");
 	window.setFramerateLimit(120);
@@ -31,31 +33,43 @@ int main()
 
 	RectangleShape volume(Vector2f(200, 20));
 	volume.setPosition(850, 540);
-	
 
-	Texture Tbackground, Tkey, Tcoin, tbutton, tslider;
+
+
+	Texture Tbackground, Tkey, Tcoin, tbutton, opendoor, tslider;
 	Tbackground.loadFromFile("SFMLProject2.png");
 	Tkey.loadFromFile("key2.png");
 	Tcoin.loadFromFile("spinning coin2.png");
 	tbutton.loadFromFile("button_ui.png");
+	opendoor.loadFromFile("opened door.png");
 	tslider.loadFromFile("slider.png");
 
-	Sprite slider(tslider), background(Tbackground), key(Tkey, IntRect(0, 0, 39, 39)), coin(Tcoin, IntRect(0, 0, 26, 22));
+
+	Sprite slider(tslider), background(Tbackground), key(Tkey, IntRect(0, 0, 39, 39)), coin(Tcoin, IntRect(0, 0, 26, 22)), door(opendoor);
 	background.setPosition(Vector2f(0, 250));
 	key.setPosition(790, 555);
 	coin.setPosition(1290, 590);
+	door.setPosition(1224.2, 530.4);
+	door.scale(2.14, 2.14);
 	slider.setPosition(850, 540);
-
 	float xKey = 0, yKey = 0, delayKey = 0.075;
 	float xCoin = 0, yCoin = 0, delayCoin = 0.065;
-	
-	Sprite button [4];
+	float f = 0, delay = 0.09f, g = 9.9, v = 7.9, jumpv = 0;
+	bool isground = 0;
+
+	Clock time;
+	Texture texture;
+	Sprite player(texture, IntRect(0, 0, 1200 / 10, 80));
+	player.setPosition(Vector2f(0, 500));
+	player.scale(2, 2);
+	Sprite button[4];
 	for (int i = 0; i < 3; i++)
 	{
 		button[i].setTexture(tbutton);
 		button[i].setScale(2, 2);
 		button[i].setPosition(850, 400 + 200 * i);
 	}
+
 	button[3].setTexture(tbutton);
 	button[3].setScale(2, 2);
 	button[3].setPosition(400, 520);
@@ -63,8 +77,10 @@ int main()
 	Font font;
 	font.loadFromFile("Nightmare_Before_Christmas.ttf");
 
+
+
 	Text Menu[MENU_SIZE];
-	
+
 	for (int i = 0; i < MENU_SIZE; i++)
 	{
 		Menu[i].setFont(font);
@@ -73,6 +89,7 @@ int main()
 		Menu[i].setPosition((i == 0 ? 910 : 900), 400 + 200 * i);
 		Menu[i].setFillColor(Color::White);
 	}
+
 	Text volumeb;
 	volumeb.setFont(font);
 	volumeb.setCharacterSize(60);
@@ -80,16 +97,17 @@ int main()
 	volumeb.setPosition(435, 520);
 	volumeb.setFillColor(Color::Green);
 
-
 	Music music;
 	music.openFromFile("Dame Tu Tormento.wav");
 	music.play();
+	music.setLoop(true);
 
 	int cnt = 0, select = 1;
 	bool started = false, paused = false, options_status = 0;
-
+	bool keycollect = 0;
 	while (window.isOpen())
 	{
+
 		window.clear();
 		Event event;
 		while (window.pollEvent(event))
@@ -98,11 +116,10 @@ int main()
 				window.close();
 		}
 
-		if (!started) 
+		if (!started)
 		{
 			if (options_status)
 			{
-
 				window.clear();
 				window.draw(slider);
 				window.draw(volume);
@@ -111,8 +128,8 @@ int main()
 				if (Keyboard::isKeyPressed(Keyboard::Key::Escape))
 					options_status = false;
 
-				if(Mouse::isButtonPressed(Mouse::Left))
-				{ 
+				if (Mouse::isButtonPressed(Mouse::Left))
+				{
 
 					Vector2i mousePos = Mouse::getPosition(window);
 					if (mousePos.x >= 850 && mousePos.x <= 1050 && mousePos.y >= 540 && mousePos.y <= 550)
@@ -123,7 +140,7 @@ int main()
 				}
 			}
 
-			else if (select == 1) 
+			else if (select == 1)
 			{
 				Menu[START_BUTTON].setFillColor(Color::Red);
 				Menu[OPTIONS_BUTTON].setFillColor(Color::White);
@@ -136,7 +153,7 @@ int main()
 					Menu[START_BUTTON].setPosition(890, 400);
 				}
 			}
-			else if (select == 2) 
+			else if (select == 2)
 			{
 				Menu[OPTIONS_BUTTON].setFillColor(Color::Red);
 				Menu[START_BUTTON].setFillColor(Color::White);
@@ -146,27 +163,27 @@ int main()
 					options_status = true;
 
 			}
-			else if (select == 3) 
+			else if (select == 3)
 			{
 				Menu[EXIT_BUTTON].setFillColor(Color::Red);
 				Menu[START_BUTTON].setFillColor(Color::White);
 				Menu[OPTIONS_BUTTON].setFillColor(Color::White);
 
-				if (Keyboard::isKeyPressed(Keyboard::Key::Enter)) 
+				if (Keyboard::isKeyPressed(Keyboard::Key::Enter))
 					window.close();
 
 			}
 			if (select > 3) select = 1;
 			if (select < 1) select = 3;
 
-			if (Keyboard::isKeyPressed(Keyboard::Key::Down)) 
+			if (Keyboard::isKeyPressed(Keyboard::Key::Down))
 			{
 				if (cnt == 0)
 					select++;
 				cnt++;
 
 			}
-			else if (Keyboard::isKeyPressed(Keyboard::Key::Up)) 
+			else if (Keyboard::isKeyPressed(Keyboard::Key::Up))
 			{
 				if (cnt == 0)
 					select--;
@@ -182,13 +199,13 @@ int main()
 			}
 		}
 
+
+
 		else if (started && !paused)
 		{
 			if (Keyboard::isKeyPressed(Keyboard::Key::Escape))
-			{
-				paused = true;
-				continue;
-			}
+				paused = 1;
+
 
 			xKey += delayKey;
 			key.setTextureRect(IntRect(short(xKey) * 39, short(yKey) * 39, 39, 39));
@@ -207,11 +224,137 @@ int main()
 			if (xCoin >= 5)
 				xCoin = 0;
 
+			if (Keyboard::isKeyPressed(Keyboard::Key::D) && !(Keyboard::isKeyPressed(Keyboard::Key::C))) {
+				if (Keyboard::isKeyPressed(Keyboard::Key::Space) && isground == 1) {
+					jumpv = 8;
+					player.move(3.0f, 0);
+					isground = 0;
+				}
+				else {
+					player.setTextureRect(IntRect((short int)f * 120, 0, 1200 / 10, 80));
+					f += delay;
+					texture.loadFromFile("_Run.png");
+					player.move(2.0f, 0);
+				}
+			}
+			else if (Keyboard::isKeyPressed(Keyboard::Key::A) && !(Keyboard::isKeyPressed(Keyboard::Key::C))) {
+				if (Keyboard::isKeyPressed(Keyboard::Key::Space) && isground == 1) {
+					jumpv = 8;
+					player.move(-3.0f, 0);
+					isground = 0;
+				}
+				else {
+					g -= delay;
+					texture.loadFromFile("Runflip.png");
+					player.move(-2.0f, 0);
+					player.setTextureRect(IntRect((short int)g * 120, 0, 1200 / 10, 80));
+					if (g < 0.1)
+						g = 10;
+				}
+			}
+			else if (Keyboard::isKeyPressed(Keyboard::Key::C) && Keyboard::isKeyPressed(Keyboard::Key::D)) {
+				player.setTextureRect(IntRect((short int)f * 120, 0, 1200 / 10, 80));
+				f += delay;
+				texture.loadFromFile("CrouchWalk.png");
+				player.move(0.5f, 0);
+				if (f > 7.9) {
+					f = 0;
+				}
+			}
+			else if (Keyboard::isKeyPressed(Keyboard::Key::C) && Keyboard::isKeyPressed(Keyboard::Key::A)) {
+				player.setTextureRect(IntRect((short int)v * 120, 0, 1200 / 10, 80));
+
+				v -= delay;
+				texture.loadFromFile("CrouchWalkflip.png");
+				player.move(-0.5f, 0);
+				if (v < 0.1) {
+					v = 7.9;
+				}
+			}
+			else if (Keyboard::isKeyPressed(Keyboard::Key::C)) {
+				texture.loadFromFile("Crouch.png");
+				player.setTextureRect(IntRect((short int)0, 0, 1200 / 10, 80));
+			}
+			else if (Keyboard::isKeyPressed(Keyboard::Key::Space) && isground == 1) {
+				jumpv = 8;
+				isground = 0;
+			}
+			else {
+				texture.loadFromFile("_Idle.png");
+				player.setTextureRect(IntRect((short int)f * 120, 0, 1200 / 10, 80));
+				f += delay;
+			}
+			if (player.getGlobalBounds().intersects(coin.getGlobalBounds())) {
+				coin.setPosition(Vector2f(10000, 10000));
+			}
+
+			if (player.getGlobalBounds().intersects(key.getGlobalBounds())) {
+				keycollect = 1;
+				key.setPosition(Vector2f(10000, 10000));
+			}
+
+
+			if (jumpv > 0) {
+				player.setPosition(player.getPosition().x, player.getPosition().y - 10.2f);
+				jumpv -= 4.8 * time.getElapsedTime().asSeconds();
+				texture.loadFromFile("Jump.png");
+				player.setTextureRect(IntRect((short int)f * 120, 0, 360 / 3, 80));
+				f += delay;
+				if (f > 2.9)
+					f = 0;
+			}
+			else {
+				jumpv = 0;
+				time.restart();
+			}
+			if (player.getGlobalBounds().intersects(ground1.getGlobalBounds()) ||
+				player.getGlobalBounds().intersects(ground2.getGlobalBounds()) ||
+				player.getGlobalBounds().intersects(ground3.getGlobalBounds()) ||
+				player.getGlobalBounds().intersects(ground4.getGlobalBounds())) {
+				player.setPosition(player.getPosition().x, player.getPosition().y - 0.001);
+				isground = 1;
+			}
+
+			else if (jumpv <= 0) {
+				isground = 0;
+				player.setPosition(player.getPosition().x, player.getPosition().y + 4.8);
+				if (Keyboard::isKeyPressed(Keyboard::Key::D)) {
+					texture.loadFromFile("_Fall.png");
+					player.setTextureRect(IntRect((short int)f * 120, 0, 360 / 3, 80));
+					f += delay;
+					if (f > 2.9)
+						f = 0;
+				}
+				else if (Keyboard::isKeyPressed(Keyboard::Key::A)) {
+					texture.loadFromFile("Fallflip.png");
+					player.setTextureRect(IntRect((short int)f * 120, 0, 360 / 3, 80));
+					f += delay;
+					if (f > 2.9)
+						f = 0;
+				}
+				else {
+					texture.loadFromFile("_Fall.png");
+					player.setTextureRect(IntRect((short int)f * 120, 0, 360 / 3, 80));
+					f += delay;
+					if (f > 2.9)
+						f = 0;
+				}
+
+			}
+			//reset value
+			if (f > 9.9) {
+				f = 0;
+			}
+
 			window.draw(background);
 			window.draw(key);
 			window.draw(coin);
+
+			if (player.getGlobalBounds().intersects(door.getGlobalBounds()) && keycollect == 1)
+				window.draw(door);
+			window.draw(player);
 		}
-		
+
 		else if (paused && started)
 		{
 
@@ -236,18 +379,17 @@ int main()
 					}
 				}
 			}
-
-			if (select == 1) 
+			if (select == 1)
 			{
 
 				Menu[START_BUTTON].setFillColor(Color::Red);
 				Menu[OPTIONS_BUTTON].setFillColor(Color::White);
 				Menu[EXIT_BUTTON].setFillColor(Color::White);
 
-				if (Keyboard::isKeyPressed(Keyboard::Key::Enter)) 
+				if (Keyboard::isKeyPressed(Keyboard::Key::Enter))
 					paused = 0;
 			}
-			else if (select == 2) 
+			else if (select == 2)
 			{
 				Menu[OPTIONS_BUTTON].setFillColor(Color::Red);
 				Menu[START_BUTTON].setFillColor(Color::White);
@@ -256,13 +398,13 @@ int main()
 				if (Keyboard::isKeyPressed(Keyboard::Key::Enter))
 					options_status = true;
 			}
-			else if (select == 3) 
+			else if (select == 3)
 			{
 				Menu[EXIT_BUTTON].setFillColor(Color::Red);
 				Menu[START_BUTTON].setFillColor(Color::White);
 				Menu[OPTIONS_BUTTON].setFillColor(Color::White);
 
-				if (Keyboard::isKeyPressed(Keyboard::Key::Enter)) 
+				if (Keyboard::isKeyPressed(Keyboard::Key::Enter))
 					window.close();
 			}
 			if (select > 3) select = 1;
@@ -283,9 +425,12 @@ int main()
 
 			if (!options_status)
 			{
+
+
 				window.draw(background);
 				window.draw(key);
 				window.draw(coin);
+
 
 				for (int i = 0; i < 3; i++)
 					window.draw(button[i]);
